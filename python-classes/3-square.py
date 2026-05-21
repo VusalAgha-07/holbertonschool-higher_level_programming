b6db476955a6a9594ac1c6442589f5aa5f398c57
vusalagha07 <svusalagha1@gmail.com>
2026-05-21 22:26:53 +0000

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -19,4 +19,4 @@ class Square:
 
     def area(self):
         """Method that returns the current area of the square."""
-        return self.__size * self.__size 
+        return self.__size * self.__size
