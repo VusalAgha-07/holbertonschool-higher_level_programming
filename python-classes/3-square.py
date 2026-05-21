f465e06140d7ead7475837a054aede19384b3466
vusalagha07 <svusalagha1@gmail.com>
2026-05-21 22:12:06 +0000

    Task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with area calculation."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes the square with size validation.
+
+        Args:
+            size (int): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Method that returns the current area of the square."""
+        return self.__size * self.__size 
