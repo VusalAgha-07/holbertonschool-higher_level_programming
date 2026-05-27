9fa092cd38a448768d144f1e176a45092f63f398
vusalagha07 <vusalagha@gmail.com>
2026-05-27 23:29:28 +0000

    Task 4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table in the current database
+-- with columns id (INT) and name (VARCHAR(156))
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+	);
