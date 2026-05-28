61dcfa606684bf614b2476a610f3822d36cff160
vusalagha07 <vusalagha@gmail.com>
2026-05-28 00:55:07 +0000

    Task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database.
+It shows all states from 'hbtn_0e_0_usa' and sorts them by ID.
+"""
+import sys
+import MySQLdb
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    db.close()
