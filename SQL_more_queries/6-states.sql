dfbf30efa30d1a8c17f338fa5252ce3aa6e22020
vusalagha07 <vusalagha@gmail.com>
2026-05-28 00:29:24 +0000

    Task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0d_usa
+-- Create table states with primary key and auto increment
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
